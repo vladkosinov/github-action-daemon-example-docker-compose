38284438c20aebd5029cbf9b890a2c29232bd4e1
Vlad Kosinov <vladkosinov@gmail.com>
2020-11-03 00:16:35 +0100

    fix localtunnel-live.sh

--- localtunnel-live.sh
+++ localtunnel-live.sh
@@ -7,9 +7,10 @@ while read -r; do
     if [ $URL ]; then
         sed -i -E "s~Live url.*$~Live url: $URL~g" README.md;
         git add README.md;
-        git commit -m "Update Live URL" \
-           -c user.name="Github Actions" \
-           -c user.email="$GITHUB_ACTOR@users.noreply.github.com";
+        git -c user.name="Github Actions" \
+            -c user.email="$GITHUB_ACTOR@users.noreply.github.com" \
+            commit -m "Update Live URL";
+
         git push origin HEAD;
     fi;
 done;
