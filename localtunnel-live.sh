2b1c3e4b3cac2b7bb78ca2683eed7170bb345eba
Vlad Kosinov <vladkosinov@gmail.com>
2020-11-03 00:00:41 +0100

    Add localtunnel-live.sh

---
+++ localtunnel-live.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env
+
+npx supervisor -i . -- "$(which npx)" localtunnel --port 3000 | \
+while read -r; do
+    echo $REPLY;
+    URL=$(echo $REPLY | grep "url is" | sed -E 's/.*url is.*(http.*)$/\1/')
+    if [ $URL ]; then
+        sed -i -E "s~Live url.*$~Live url: $URL~g" README.md;
+        git add README.md;
+        git commit -m "Update Live URL" \
+           -c user.name="Github Actions" \
+           -c user.email='${{ github.actor }}@users.noreply.github.com';
+        git push origin HEAD;
+    fi;
+done;
